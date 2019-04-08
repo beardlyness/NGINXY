2613201bdcbdee918597e99a154c83a5d31490dc
Beard <beardlyness@users.noreply.github.com>
2019-04-08 13:38:19 -0700

    V :0.1.1 Beta
    # title            :NGINXY
    # description      :This script will make it super easy to setup a Reverse Proxy with NGINX.
    # author           :The Crypto World Foundation.
    # contributors     :beard
    # date             :04-08-2019
    # version          :0.1.1 Beta
    # os               :Debian/Ubuntu
    # usage            :bash nginxy.sh
    # notes            :If you have any problems feel free to email the maintainer: beard [AT] cryptoworld [DOT] is

--- nginxy.sh
+++ nginxy.sh
@@ -19,15 +19,15 @@
 # description      :This script will make it super easy to setup a Reverse Proxy with NGINX.
 # author           :The Crypto World Foundation.
 # contributors     :beard
-# date             :04-06-2019
-# version          :0.1.0 Beta
+# date             :04-08-2019
+# version          :0.1.1 Beta
 # os               :Debian/Ubuntu
 # usage            :bash nginxy.sh
 # notes            :If you have any problems feel free to email the maintainer: beard [AT] cryptoworld [DOT] is
 #===============================================================================================================================================
 
 # Force check for root
-  if ! [ $(id -u) = 0 ]; then
+  if ! [ "$(id -u)" = 0 ]; then
     echo "You need to be logged in as root!"
     exit 1
   fi
@@ -42,15 +42,15 @@
 
   # Setting up different NGINX branches to prep for install
     function nginx_stable() {
-        echo deb http://nginx.org/packages/$system/ $flavor nginx > /etc/apt/sources.list.d/$flavor.nginx.stable.list
-        echo deb-src http://nginx.org/packages/$system/ $flavor nginx >> /etc/apt/sources.list.d/$flavor.nginx.stable.list
+        echo deb http://nginx.org/packages/"$system"/ "$flavor" nginx > /etc/apt/sources.list.d/"$flavor".nginx.stable.list
+        echo deb-src http://nginx.org/packages/"$system"/ "$flavor" nginx >> /etc/apt/sources.list.d/"$flavor".nginx.stable.list
           wget https://nginx.org/keys/nginx_signing.key
           apt-key add nginx_signing.key
       }
 
     function nginx_mainline() {
-        echo deb http://nginx.org/packages/mainline/$system/ $flavor nginx > /etc/apt/sources.list.d/$flavor.nginx.mainline.list
-        echo deb-src http://nginx.org/packages/mainline/$system/ $flavor nginx >> /etc/apt/sources.list.d/$flavor.nginx.mainline.list
+        echo deb http://nginx.org/packages/mainline/"$system"/ "$flavor" nginx > /etc/apt/sources.list.d/"$flavor".nginx.mainline.list
+        echo deb-src http://nginx.org/packages/mainline/"$system"/ "$flavor" nginx >> /etc/apt/sources.list.d/"$flavor".nginx.mainline.list
           wget https://nginx.org/keys/nginx_signing.key
           apt-key add nginx_signing.key
       }
@@ -67,32 +67,36 @@
             wget -O /etc/security/limits.conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/security/limits.conf
           echo "Setting up background NGINX workers.."
             wget -O /etc/default/nginx https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/default/nginx
-            echo "Setting up configuration file for NGINX main configuration.."
-              wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/nginx/nginx.conf
+          echo "Setting up configuration file for NGINX main configuration.."
+            wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/nginx/nginx.conf
           echo "Setting up folders.."
-            mkdir -p /etc/engine/ssl/live
-            mkdir -p /var/www/html/proxy/live
         }
-
 
         function proxy_default()  {
           read -r -p "Domain Name: (Leave { HTTPS:/// | HTTP:// | WWW. } out of the domain) " DOMAIN
-            if [[ "${DOMAIN,,}" ]]
+            if [[ -n "${DOMAIN,,}" ]]
               then
                 echo "Setting up configuration file for NGINX Proxy.."
-                  wget -O /etc/nginx/conf.d/nginx-proxy.conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/nginx/conf.d/nginx-proxy.conf
+                  wget -O /etc/nginx/conf.d/"$DOMAIN".conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/nginx/conf.d/nginx-proxy.conf
                 echo "Changing 'server_name foobar' >> server_name '$DOMAIN' .."
-                  sed -i 's/server_name foobar/server_name '$DOMAIN'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                  sed -i 's/server_name foobar/server_name '"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
+                echo "Fixing up the site configuration file for NGINX.."
+                  sed -i 's/domain/'"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                 echo "Domain Name has been set to: '$DOMAIN' "
                 echo "Removing Default NGINX Configuration files.."
                   mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.old
+                echo "Setting up folders.."
+                  mkdir -p /etc/engine/ssl/"$DOMAIN"
+                  mkdir -p /var/www/html/"$DOMAIN"/live
+              else
+                echo "You have entered an invalid Domain Name."
             fi
 
             read -r -p "Please enter the IP Address for the Backend IP: " IPA
               if [[ "${IPA},,}" =~ (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) ]]
                 then
                   echo "Changing 'server Main-A' >> server '$IPA' .."
-                    sed -i 's/$backend/'$IPA'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                    sed -i 's/backend/'"$IPA"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                   echo "Backend IP Address has been set to: '$IPA' "
                 else
                   echo "You have entered an invalid IP Address.."
@@ -101,22 +105,29 @@
 
         function proxy_upstream() {
           read -r -p "Domain Name: (Leave { HTTPS:/// | HTTP:// | WWW. } out of the domain) " DOMAIN
-            if [[ "${DOMAIN,,}" ]]
+            if [[ -n "${DOMAIN,,}" ]]
               then
                 echo "Setting up configuration file for NGINX Proxy.."
-                  wget -O /etc/nginx/conf.d/nginx-proxy.conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/nginx/conf.d/nginx-upstream.conf
+                  wget -O /etc/nginx/conf.d/"$DOMAIN".conf https://raw.githubusercontent.com/beardlyness/NGINXY/master/etc/nginx/conf.d/nginx-upstream.conf
                 echo "Changing 'server_name foobar' >> server_name '$DOMAIN' .."
-                  sed -i 's/server_name foobar/server_name '$DOMAIN'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                  sed -i 's/server_name foobar/server_name '"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
+                echo "Fixing up the site configuration file for NGINX.."
+                  sed -i 's/domain/'"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                 echo "Domain Name has been set to: '$DOMAIN' "
                 echo "Removing Default NGINX Configuration files.."
                   mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.old
+                echo "Setting up folders.."
+                  mkdir -p /etc/engine/ssl/"$DOMAIN"
+                  mkdir -p /var/www/html/"$DOMAIN"/live
+              else
+                echo "Sorry we cannot live on! RIP Dead.."
             fi
 
             read -r -p "Please enter the IP Address for Upstream IP: " IPA
               if [[ "${IPA},,}" =~ (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) ]]
                 then
                   echo "Changing 'server Main-A' >> server '$IPA' .."
-                    sed -i 's/server Main-A/server '$IPA'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                    sed -i 's/server Main-A/server '"$IPA"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                   echo "Upstream IP Address has been set to: '$IPA' "
                 else
                   echo "You have entered an invalid IP Address.."
@@ -126,7 +137,7 @@
                 if [[ "${IPB},,}" =~ (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) ]]
                   then
                     echo "Changing 'server Main-B' >> server '$IPB' .."
-                      sed -i 's/server Main-B/server '$IPB'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                      sed -i 's/server Main-B/server '"$IPB"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                     echo "Upstream IP Address has been set to: '$IPB' "
                   else
                     echo "You have entered an invalid IP Address.."
@@ -136,7 +147,7 @@
                   if [[ "${IPC},,}" =~ (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) ]]
                     then
                       echo "Changing 'server Main-C' >> server '$IPC' .."
-                        sed -i 's/server Main-C/server '$IPC'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                        sed -i 's/server Main-C/server '"$IPC"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                       echo "Upstream IP Address has been set to: '$IPC' "
                     else
                       echo "You have entered an invalid IP Address.."
@@ -146,7 +157,7 @@
                     if [[ "${IPD},,}" =~ (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?) ]]
                       then
                         echo "Changing 'server Main-D' >> server '$IPD' .."
-                          sed -i 's/server Main-D/server '$IPD'/g' /etc/nginx/conf.d/nginx-proxy.conf
+                          sed -i 's/server Main-D/server '"$IPD"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
                         echo "Upstream IP Address has been set to: '$IPD' "
                       else
                         echo "You have entered an invalid IP Address.."
@@ -156,11 +167,11 @@
         #Prep for Custom Error Page Handling
           function custom_errors() {
             echo "Setting up folders.."
-              mkdir -p /var/www/html/proxy/live/errors
+              mkdir -p /var/www/html/"$DOMAIN"/live/errors
             echo "Grabbing Custom Error Pages & Handling from GitHub.."
-              wget https://github.com/beardlyness/nginxy-custom-errors/archive/master.tar.gz -O - | tar -xz -C /var/www/html/proxy/live/errors/  && mv /var/www/html/proxy/live/errors/NGINXY-Custom-Errors-master/* /var/www/html/proxy/live/errors/
+              wget https://github.com/beardlyness/nginxy-custom-errors/archive/master.tar.gz -O - | tar -xz -C /var/www/html/"$DOMAIN"/live/errors/  && mv /var/www/html/"$DOMAIN"/live/errors/NGINXY-Custom-Errors-master/* /var/www/html/"$DOMAIN"/live/errors/
             echo "Removing temporary files/folders.."
-              rm -rf /var/www/html/proxy/live/NGINXY-Custom-Errors-master && rm -rf /var/www/html/proxy/live/errors/LICENSE
+              rm -rf /var/www/html/"$DOMAIN"/live/NGINXY-Custom-Errors-master && rm -rf /var/www/html/"$DOMAIN"/live/errors/LICENSE
           }
 
         #Prep for SSL setup & install via ACME.SH script | Check it out here: https://github.com/Neilpang/acme.sh
@@ -169,22 +180,22 @@
                   wget -O -  https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh | INSTALLONLINE=1  sh
                   reset
                   service nginx stop
-                  openssl dhparam -out /etc/engine/ssl/live/dhparam.pem 2048
-                  bash ~/.acme.sh/acme.sh --issue --standalone -d $DOMAIN -ak 4096 -k 4096 --force
-                  bash ~/.acme.sh/acme.sh --install-cert -d $DOMAIN \
-                    --key-file    /etc/engine/ssl/live/ssl.key \
-                    --fullchain-file    /etc/engine/ssl/live/certificate.cert \
+                  openssl dhparam -out /etc/engine/ssl/"$DOMAIN"/dhparam.pem 2048
+                  bash ~/.acme.sh/acme.sh --issue --standalone -d "$DOMAIN" -ak 4096 -k 4096 --force
+                  bash ~/.acme.sh/acme.sh --install-cert -d "$DOMAIN" \
+                    --key-file    /etc/engine/ssl/"$DOMAIN"/ssl.key \
+                    --fullchain-file    /etc/engine/ssl/"$DOMAIN"/certificate.cert \
                     --reloadcmd   "service nginx restart"
           }
 
           #Prep for SSL setup for Qualys rating
           function sslqualy() {
             echo "Preparing to setup NGINX to meet Qualys 100% Standards.."
-              sed -i 's/ssl_prefer_server_ciphers/#ssl_prefer_server_ciphers/g' /etc/nginx/conf.d/nginx-proxy.conf
-              sed -i 's/#ssl_ciphers/ssl_ciphers/g' /etc/nginx/conf.d/nginx-proxy.conf
-              sed -i 's/#ssl_ecdh_curve/ssl_ecdh_curve/g' /etc/nginx/conf.d/nginx-proxy.conf
+              sed -i 's/ssl_prefer_server_ciphers/#ssl_prefer_server_ciphers/g' /etc/nginx/conf.d/"$DOMAIN".conf
+              sed -i 's/#ssl_ciphers/ssl_ciphers/g' /etc/nginx/conf.d/"$DOMAIN".conf
+              sed -i 's/#ssl_ecdh_curve/ssl_ecdh_curve/g' /etc/nginx/conf.d/"$DOMAIN".conf
             echo "Generating a 4096 DH Param. This may take a while.."
-              openssl dhparam -out /etc/engine/ssl/live/dhparam.pem 4096
+              openssl dhparam -out /etc/engine/ssl/"$DOMAIN"/dhparam.pem 4096
             echo "Restarting NGINX Service..."
               service nginx restart
           }
@@ -193,7 +204,7 @@
         function phpdev() {
           echo "Setting up PHP Branches for install.."
             wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
-          echo "deb https://packages.sury.org/php/ $flavor main" | tee /etc/apt/sources.list.d/php.list
+          echo "deb https://packages.sury.org/php/ ""$flavor"" main" | tee /etc/apt/sources.list.d/php.list
         }
 
 
@@ -237,8 +248,8 @@
     fi
 
     # Grabbing info on active machine.
-        flavor=`lsb_release -cs`
-        system=`lsb_release -i | grep "Distributor ID:" | sed 's/Distributor ID://g' | sed 's/["]//g' | awk '{print tolower($1)}'`
+        flavor=$(lsb_release -cs)
+        system=$(lsb_release -i | grep "Distributor ID:" | sed 's/Distributor ID://g' | sed 's/["]//g' | awk '{print tolower($1)}')
 
 
 # NGINX Arg main
@@ -381,11 +392,11 @@ read -r -p "Would you like to setup the sysctl.conf to harden the security of th
               sed -i 's/listen.owner = www-data/listen.owner = nginx/g' /etc/php/7.1/fpm/pool.d/www.conf
               sed -i 's/listen.group = www-data/listen.group = nginx/g' /etc/php/7.1/fpm/pool.d/www.conf
               sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.1/fpm/php.ini
-              sed -i 's/phpx.x-fpm.sock/php7.1-fpm.sock/g' /etc/nginx/conf.d/nginx-proxy.conf
+              sed -i 's/phpx.x-fpm.sock/php7.1-fpm.sock/g' /etc/nginx/conf.d/"$DOMAIN".conf
               service php7.1-fpm restart
               service php7.1-fpm status
               service nginx restart
-              ps aux | grep -v root | grep php-fpm | cut -d\  -f1 | sort | uniq
+              pgrep -v root | pgrep php-fpm | cut -d\  -f1 | sort | uniq
             ;;
         2)
           echo "Installing PHP 7.2, and its modules.."
@@ -395,11 +406,11 @@ read -r -p "Would you like to setup the sysctl.conf to harden the security of th
               sed -i 's/listen.owner = www-data/listen.owner = nginx/g' /etc/php/7.2/fpm/pool.d/www.conf
               sed -i 's/listen.group = www-data/listen.group = nginx/g' /etc/php/7.2/fpm/pool.d/www.conf
               sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.2/fpm/php.ini
-              sed -i 's/phpx.x-fpm.sock/php7.2-fpm.sock/g' /etc/nginx/conf.d/nginx-proxy.conf
+              sed -i 's/phpx.x-fpm.sock/php7.2-fpm.sock/g' /etc/nginx/conf.d/"$DOMAIN".conf
               service php7.2-fpm restart
               service php7.2-fpm status
               service nginx restart
-              ps aux | grep -v root | grep php-fpm | cut -d\  -f1 | sort | uniq
+              pgrep -v root | pgrep php-fpm | cut -d\  -f1 | sort | uniq
             ;;
         3)
           echo "Installing PHP 7.3, and its modules.."
@@ -409,11 +420,11 @@ read -r -p "Would you like to setup the sysctl.conf to harden the security of th
              sed -i 's/listen.owner = www-data/listen.owner = nginx/g' /etc/php/7.3/fpm/pool.d/www.conf
              sed -i 's/listen.group = www-data/listen.group = nginx/g' /etc/php/7.3/fpm/pool.d/www.conf
              sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/fpm/php.ini
-             sed -i 's/phpx.x-fpm.sock/php7.3-fpm.sock/g' /etc/nginx/conf.d/nginx-proxy.conf
+             sed -i 's/phpx.x-fpm.sock/php7.3-fpm.sock/g' /etc/nginx/conf.d/"$DOMAIN".conf
              service php7.3-fpm restart
              service php7.3-fpm status
              service nginx restart
-             ps aux | grep -v root | grep php-fpm | cut -d\  -f1 | sort | uniq
+             pgrep -v root | pgrep php-fpm | cut -d\  -f1 | sort | uniq
             ;;
       esac
   clear
